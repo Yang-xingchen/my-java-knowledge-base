c42b0497f583cb8a50eb323e60db09a0532c070c
Yang-xingchen <1459177541@qq.com>
2025-03-01 02:02:27 +0800

    dynamic-datasource: 手动数据源、懒加载数据源

--- frame/dynamic-datasource/README.md
+++ frame/dynamic-datasource/README.md
@@ -2,3 +2,33 @@
 [文档](https://www.kancloud.cn/tracy5546/dynamic-datasource/2264611)
 [github](https://github.com/baomidou/dynamic-datasource)
 
+# 数据源配置
+[application.yaml](src%2Fmain%2Fresources%2Fapplication.yaml)
+```yaml
+spring:
+  datasource:
+    dynamic:
+      datasource:
+        master_1:
+          url: jdbc:mysql://192.168.31.201:3306/ds1?useUnicode=true&characterEncoding=utf8&serverTimezone=GMT%2B8
+          username: root
+          password: 123456
+        master_2:
+          url: jdbc:mysql://192.168.31.201:3306/ds2?useUnicode=true&characterEncoding=utf8&serverTimezone=GMT%2B8
+          username: root
+          password: 123456
+```
+
+# 分组
+组名为: `ds.split("_")[0]`, 同组默认采用轮询(`LoadBalanceDynamicDataSourceStrategy`)
+
+# 切换数据源
+可填写组名或具体名称。
+1. `@DS`注解
+2. DynamicDataSourceContextHolder.push
+
+优先级: DynamicDataSourceContextHolder.push > 方法上的`@DS`注解 > 类上的`@DS`注解
+
+# 懒加载
+`DynamicRoutingDataSource.addDataSource()`
+
--- frame/dynamic-datasource/src/main/java/com/example/DynamicDatasourceApplication.java
+++ frame/dynamic-datasource/src/main/java/com/example/DynamicDatasourceApplication.java
@@ -43,9 +43,34 @@ public class DynamicDatasourceApplication {
     public CommandLineRunner w1r2(UserServer userServer) {
         return args -> {
             Long id = userServer.save1("w1r2");
-            Assertions.assertNull(userServer.find2(id));
+            Assertions.assertNotEquals("w1r2", userServer.find2(id));
             log.info("w1r2: {}", id);
         };
     }
+
+//    @Bean
+//    public DataSource dataSource(DefaultDataSourceCreator creator) {
+//        DynamicDataSourceProvider provider = () -> Stream.of("1", "2").collect(Collectors.toMap(s -> "master_" + s, s -> {
+//            DataSourceProperty property = new DataSourceProperty();
+//            property.setUrl("jdbc:mysql://192.168.31.201:3306/ds" + s + "?useUnicode=true&characterEncoding=utf8&serverTimezone=GMT%2B8");
+//            property.setUsername("root");
+//            property.setPassword("123456");
+//            return creator.createDataSource(property);
+//        }));
+//        return new DynamicRoutingDataSource(List.of(provider));
+//        每次执行SQL都会创建数据源，需手动关闭
+//        return new DynamicRoutingDataSource(List.of()) {
+//            @Override
+//            public DataSource getDataSource(String ds) {
+//                log.info("create dataSource: {}", ds);
+//                String s = ds.split("_")[1];
+//                DataSourceProperty property = new DataSourceProperty();
+//                property.setUrl("jdbc:mysql://192.168.31.201:3306/ds" + s + "?useUnicode=true&characterEncoding=utf8&serverTimezone=GMT%2B8");
+//                property.setUsername("root");
+//                property.setPassword("123456");
+//                return creator.createDataSource(property);
+//            }
+//        };
+//    }
 
 }
